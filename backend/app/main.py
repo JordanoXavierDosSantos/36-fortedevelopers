5e043cc082b75878f319136ee9fef99293cda026
Jordano Xavier <jordano.xavier@fortesecurity.com.br>
2025-06-14 05:19:23 +0000

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from .models import ImovelInput
 from .ml_model import PricePredictor
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="API Previsão de Preços de Imóveis",
     description="Modelo de Regressão Linear treinado com dataset público",
     version="1.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Instancia o modelo
 predictor = PricePredictor("dataset_imoveis.csv")
